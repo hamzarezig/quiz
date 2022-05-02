3f872ef38fc50222e2c3920c38400de4b595c272
Hamza rezig <rezig3791@gmail.com>
2022-05-02 11:32:24 -0700

    Making a cheker for a register form

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const ejsMate = require('ejs-mate')
 const mongoose = require('mongoose')
+const emailvalidator = require("email-validator");
 const app = express()
 
+//those middleware will be deleted later
+const isValidRegisterForm = (req,res,next) => {
+	let email = req.body.password;
+	let username = req.body.username;
+	let password = req.body.password;
+	if(username&&password&&email){
+		if(username.replace(/\s+/g, '')!==''&&email.replace(/\s+/g, '')!==''&&password.replace(/\s+/g, '')!==''){
+			if(emailvalidator.validate(req.body.email)){
+				if(/^[a-zA-Z0-9_]{3,15}$/.test(username)){
+					next()
+				} else {
+					res.send('Username must be AlphaNumeric (Letters or Numbers), Underscore and must contain 3 to 15 characters')
+				}
+			} else {
+				res.send('Give a valid email')
+			}
+		} else {
+			res.send('Form must not be empty')
+		}
+	} else {
+		res.send('Form must not be empty')
+	}
+	next()
+}
 
+const usernameAndEmailNotInUse = (req,res,next) => {
+	//this is for testing purpeses
+	next()
+}
 
-mongoose.connect('mongodb://hamzarezig:long@quizapp.yplve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://hamzarezig:long@quizapp.yplve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(() => {
 	console.log('Mongodb connected succesfully')
 })
@@ -19,6 +48,7 @@ app.engine('ejs',ejsMate)
 app.set('views',__dirname+'/templates')
 app.set('view engine','ejs')
 
+app.use(express.urlencoded({extended:true}))
 app.use(express.static('static'))
 
 app.get('/',(req,res) => {
@@ -31,6 +61,11 @@ app.get('/login',(req,res) => {
 
 app.get('/register',(req,res) => {
 	res.render('pages/register')
+})
+
+app.post('/register',isValidRegisterForm,usernameAndEmailNotInUse,(req,res) => {
+	//
+	res.send('work')
 })
 
 app.listen(3000,() => {
